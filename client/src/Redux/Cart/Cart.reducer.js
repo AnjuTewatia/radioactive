82fcd0b8e1ef7d4441b3db832bac5805cc814608
AnjuTewatia <anjutewatia008@gmail.com>
2023-02-08 10:29:38 +0530

    added

---
+++ client/src/Redux/Cart/Cart.reducer.js
@@ -0,0 +1,8 @@
+import {ADD_ITEM_TO_CART,REMOVE_CART_ITEMS} from "./Cart.actiontypes"
+
+const initialState = {
+    cartItems:[]
+    
+}
+
+//  export const Cartreducer=
--- client/src/components/Page/Cart/Cart.jsx
+++ client/src/components/Page/Cart/Cart.jsx
@@ -2,7 +2,10 @@ import React from 'react'
 
 const Cart = () => {
   return (
-    <div>Cart</div>
+    <div>
+
+      
+    </div>
   )
 }
 
