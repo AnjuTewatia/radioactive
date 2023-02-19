e1a4d0732b0145600c50aafcae0d9a2d6615e770
AnjuTewatia <anjutewatia008@gmail.com>
2023-02-19 23:33:19 +0530

    admin

--- server/src/features/Product/product.route.js
+++ server/src/features/Product/product.route.js
@@ -6,6 +6,14 @@ const app = express.Router();
 // Get request for all products  pagination , limit , get by category ,
 //  get by category and sort by price , search by title in category
 //  and by default get all products
+app.get("/allprodAdmin", async (req, res) => {
+  try {
+    let product = await Product.find();
+    res.status(200).send(product);
+  } catch (error) {
+    return res.status(400).send(error.message);
+  }
+});
 
 app.get("", async (req, res) => {
   const { page = 1, limit = 20, category, input, priceSort } = req.query;
@@ -82,25 +90,6 @@ app.patch("/:id", async (req, res) => {
   try {
     await Product.findByIdAndUpdate({ _id: req.params.id }, payload);
     return res.status(200).send({ message: "product updated successfully  " });
-  } catch (error) {
-    return res.status(400).send(error.message);
-  }
-});
-
-app.get("/allprodAdmin", async (req, res) => {
-  const { input, category } = req.query;
-  try {
-    if (category) {
-      let product = await Product.find({ category });
-      return res.status(200).send(product);
-    } else if (input && category) {
-      let temp = new RegExp(input, "i");
-      let product = await Product.find({ title: temp });
-      return res.status(200).send(product);
-    } else {
-      let product = await Product.find();
-      return res.status(200).send(product);
-    }
   } catch (error) {
     return res.status(400).send(error.message);
   }
